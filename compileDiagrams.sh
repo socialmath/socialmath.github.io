9379fa7f796f2423bf8dc355009f12db4729c4e4
Joshua Meyers <meygerjos@gmail.com>
2017-05-04 01:34:05 -0400

    Rename and consolidate shell script and add sed command

--- compileDiagrams.sh
+++ compileDiagrams.sh
@@ -2,22 +2,28 @@
 
 cd _includes/svg/diagrams/
 
+function compile {
+    echo "Compiling $1"
+    [ -e "$1" ] || continue
+    base=${1%.lhs}
+    cabal exec -- ghc --make $filename
+    ./$base
+    # Haskell diagrams escapes text sequences when generating svg files,
+    # so we need to unescape special characters to see them by replacing
+    # "&amp;" --> "&".
+    echo "$base.svg"
+    sed -i "s/\&amp;/\&/g" "$base.svg"
+}
+
 if [[ $# -gt 0 ]]
 then for filename in "$@" ; do
-	 echo $filename
-         [ -e "$filename" ] || continue
-         base=${filename%.lhs}
-         cabal exec -- ghc --make $filename
-         ./$base
+         compile $filename
      done
     
 else for filename in [[:lower:]]*.lhs ; do
-	 echo $filename
-	 [ -e "$filename" ] || continue
-	 base=${filename%.lhs}
-	 cabal exec -- ghc --make $filename
-	 ./$base
+	 compile $filename
      done
 fi
+
 
 cd ../../../
